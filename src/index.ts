22043b5fe28a12d248b744ec592a1c94a7dccad3
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-12-17 21:32:37 -0800

    Update with simple example calling llm for addition with tool use

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,55 @@
-import { MessagesAnnotation, StateGraph, START, END } from "@langchain/langgraph";
-import fs from "node:fs/promises";
+import { StateGraph, START } from "@langchain/langgraph";
+import { AgentStateAnnotation, type GraphState } from "./lib/agent-state.js";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { makeToolRouter } from "./nodes/routers.js";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { addTool } from "./tools/examples/addition-tool.js"
+import { createLLMNode } from "./lib/llm-node.js";
+import { createLLM } from "./lib/llm-factory.js";
 
-const mockLlm = (state: typeof MessagesAnnotation.State) => {
-  return { messages: [{ role: "ai", content: "hello world" }] };
-};
+const llm = createLLM({
+  provider: "openrouter",
+  model: "anthropic/claude-3-haiku",
+  temperature: 0,
+});
 
-const graph = new StateGraph(MessagesAnnotation)
-  .addNode("mock_llm", mockLlm)
-  .addEdge(START, "mock_llm")
-  .addEdge("mock_llm", END)
+const llmNode = createLLMNode(llm, {
+  tools: [addTool],
+});
+
+
+const graph = new StateGraph(AgentStateAnnotation);
+
+const compiledGraph = graph
+  .addNode("llm", llmNode)
+  .addNode("tools", new ToolNode([addTool]))
+
+  // control flow
+  .addEdge(START, "llm")
+  .addConditionalEdges(
+    "llm",
+    makeToolRouter({ self: "llm", tools: "tools" })
+  )
+  .addEdge("tools", "llm")
   .compile();
 
-const graphData = await graph.getGraphAsync()
-const graphImage = await graphData.drawMermaidPng();
-const buffer = Buffer.from(await graphImage.arrayBuffer());
+const result = await compiledGraph.invoke({
+  agentId: "agent-1",
+  runId: "run-1",
+  startedAt: Date.now(),
+  messages: [
+    new SystemMessage(
+      "You may call tools if needed. " +
+      "When you are finished, respond with 'FINAL: <answer>' and do not continue."
+    ),
+    new HumanMessage("What is 2 + 2")
+  ],
+  objective: "Answer the math question",
+  step: 0,
+  maxSteps: 5,
+  toolHistory: [],
+  errors: [],
+});
 
-await fs.writeFile("graph.png", buffer);
-
-// await graph.invoke({ messages: [{ role: "user", content: "hi!" }] });
+console.log(result);
+console.log(result.messages.map(m => m.content));
