e858205b05b235570da32e4b8ef802c455619126
Alex Rodriguez <rodriguez.al183@gmail.com>
2025-12-17 21:30:51 -0800

    Remove old file

--- src/lib/open-router.ts
+++
@@ -1,8 +0,0 @@
-import { OpenRouter } from '@openrouter/sdk';
-import { Config } from './config.js';
-
-export const openRouter = new OpenRouter({
-  apiKey: Config.getOpenRouterApiKey(),
-  xTitle: '<YOUR_SITE_NAME>',
-  httpReferer: '<YOUR_SITE_NAME>',
-});
